f440f7d6299ea878f43596d89163ac4437e0da04
Nivretta <nivretta@gmail.com>
2016-12-06 00:12:51 -0800

    3 unit tests for fbind

--- tests/testthat/test_fbind.R
+++ tests/testthat/test_fbind.R
@@ -9,3 +9,21 @@ test_that("fbind binds factor (or character)", {
   expect_identical(fbind(x, y), z)
   expect_identical(fbind(x_fact, y), z)
 })
+
+test_that("fbind handles NAs", {
+  x <- c(1, NA)
+  y <- c('a', 'b')
+
+  z <- factor(c(1, NA, 'a', 'b'))
+
+  expect_identical(fbind(x, y), z)
+})
+
+test_that("fbind outputs factor", {
+  x <- c('a', 'b')
+  y <- c('c', 'd')
+
+  z <- (c('a', 'b', 'c', 'd'))
+
+  expect_failure(expect_identical(fbind(x, y), z))
+})
