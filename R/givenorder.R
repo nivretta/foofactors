a4d8309875c370e9765b04f32e238e95b832017c
Nivretta <nivretta@gmail.com>
2016-12-05 20:32:06 -0800

    add givenorder()

---
+++ R/givenorder.R
@@ -0,0 +1,11 @@
+#' A version of factor() that sets levels to the order in which they appear in the data,
+#' i.e. set the levels “as is”
+#'
+#' @param m factor
+#' @return factor
+#' @export
+#' @examples
+
+givenorder <- function(m) {
+  factor(m, levels = unique(m))
+}
